6dce0bc39591ff49b612fdf781bf79d127ccec5b
Luis Zabala <luis.zabala076@gmail.com>
2019-02-18 20:09:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,19 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = self.new
+    student.id, student.name, student.grade = row[0], row[1], row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    DB[:conn].execute(
+      "SELECT * FROM students").collect {|rows| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    row = DB[:conn].execute("SELECT * FROM students WHERE name=?",name).first
+    student = self.new_from_db(row)
   end
   
   def save
@@ -40,4 +42,26 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    DB[:conn].execute("SELECT * FROM students WHERE grade=9")
+  end
+  
+  def self.students_below_12th_grade
+    DB[:conn].execute("SELECT * FROM students WHERE grade < 12").collect {|row| self.new_from_db(row)}
+  end
+  
+  def self.all 
+    DB[:conn]
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
