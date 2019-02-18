e7f7b85995e5ce4296d6ae8c16cede7baa6aeaf4
Luis Zabala <luis.zabala076@gmail.com>
2019-02-18 20:49:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,8 +51,8 @@ class Student
     DB[:conn].execute("SELECT * FROM students WHERE grade < 12").collect {|row| self.new_from_db(row)}
   end
   
-  def self.first_X_students_in_grade_10(num)
-    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?", num).collect {|row| self.new_from_db(row)}
+  def self.first_X_students_in_grade_10(integer)
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?", integer).collect {|row| self.new_from_db(row)}
   end
 end
 
