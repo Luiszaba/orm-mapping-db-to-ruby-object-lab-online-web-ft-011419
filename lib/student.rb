88bc894e10d0ec2cc7fd93cefc6bf587fda33933
Luis Zabala <luis.zabala076@gmail.com>
2019-02-18 20:34:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
   end
   
   def self.first_x_students_in_grade_10
-    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?", num ).collect {|row| self.new_from_db(row)} 
+    DB[:conn].execute("SELECT * FROM students WHERE grade = 10 LIMIT ?", num ).collect {|row| self.new_from_db(rows)} 
   end
 end  
   
